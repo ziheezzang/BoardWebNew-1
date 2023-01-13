d91530fc5a5e56e63a33afb0f6559b032e4f3f5b
JiHee <xkakak0213@naver.com>
2023-01-13 18:06:09 +0900

    [add] 수정

--- src/test/java/com/ezen/biz/JoinTest.java
+++ src/test/java/com/ezen/biz/JoinTest.java
@@ -6,8 +6,7 @@ public class JoinTest {
 	public void joinTest() {
 		System.out.println("feature/1 브랜치를 위한 테스트 코드.");
 		System.out.println("issue#1에 구현하기로 한 회원가입을 구현한다.");
-		System.out.println("feature/1 브랜치를 위한 테스트 코드.");
-		System.out.println("issue#1에 구현하기로 한 회원가입을 구현한다.");
+		
 	}
 
 }
